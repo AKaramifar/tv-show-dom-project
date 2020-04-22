686ad2c9c742686a323be97e92ea3dce83cbd868
Afshin <a.karamifar@gmail.com>
2020-04-22 20:23:52 +0100

    I finished level 100

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function titleCodeGenerator(episode){
+  let seasonCode = (episode.season < 10) ? '0' + episode.season : episode.season;
+  let episodeCode = (episode.number < 10) ? '0' + episode.number : episode.number;
+  return `S${seasonCode}E${episodeCode}`;
+}
+
+function pureSummary(episode){
+  let summaryStr = (episode.summary).replace(/<p>/g,'');
+  return (summaryStr).replace(/<\/p>/g,'');
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode, index)=>{
+    rootElem.innerHTML += `
+    <div id="${titleCodeGenerator(episode)}" class="episodeDiv">
+      <p class="episodeTitle">${episode.name} - ${titleCodeGenerator(episode)}</p>
+      <img class="episodeImg" src=${episode.image.medium}>
+      <p class="summary"><strong>Summary:</strong><br>${pureSummary(episode)}</p>
+    </div>`    
+  });  
 }
 
 window.onload = setup;
