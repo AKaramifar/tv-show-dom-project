5a1b4d11169c5be3dae9f866da4641edf6a5eac1
Afshin <a.karamifar@gmail.com>
2020-04-30 01:36:12 +0100

    Level 400 first update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Declare Html Elements
 let body_El;
 let searchDiv_El;
-let searchDivSelect_El;
+let showsDivSelect_El;
+let showsListSelect_El;
+let episodeDivSelect_El;
 let episodeListSelect_El;
 let searchDivInput_El;
 let episodeInput_El;
@@ -10,16 +12,60 @@ let mainDiv_El;
 let nothingToShow_El;
 
 // Declare Objects and arrays
-let allEpisodes;
+let allShowsList;
+let allEpisodesList = [];
+let objectContainer;
 let FoundIndexArray = [];
 let replacedObject = [];
+let currentShowId = 1;
+
+function getAllEpisodes(){  
+  let showsIdList = allShowsList.map(el=> el.id);
+  showsIdList = JSON.parse(JSON.stringify(showsIdList))
+  let showsId = 0;
+
+  function getEpisodes(){
+    if(showsId < showsIdList.length){
+      fetch(`https://api.tvmaze.com/shows/${showsIdList[showsId]}/episodes`)
+      .then(Response => {
+        return Response.json()
+      })
+      .then(episodeData => {      
+        episodeData.forEach(el=>{
+          el.showId = showsIdList[showsId];
+          allEpisodesList.push(el);          
+        })    
+        showsId++;
+        getEpisodes();       
+      })
+      .catch(error => console.log(error))
+    }
+    else{
+      allEpisodesList = JSON.parse(JSON.stringify(allEpisodesList));
+      makePageForEpisodes(allEpisodesList.filter(ep => ep.showId == 1), 'load');
+      makeComboBoxOfEpisodesName(allEpisodesList.filter(el => el.showId == currentShowId));      
+    }  
+  }
+  getEpisodes();
+  
+  // //-------------------------------------------------------
+  
+}
 
 // First function which will load in may page
 function setup() {
-  allEpisodes = getAllEpisodes();  
-  myBody();
-  makePageForEpisodes(allEpisodes, 'load');
-  makeComboBoxOfEpisodesName(allEpisodes);
+  fetch('https://api.tvmaze.com/shows')
+  .then(Response => {
+    return Response.json()
+  })
+  .then(data => {
+    // Get all Shows API from URL through fetch
+    allShowsList = JSON.parse(JSON.stringify(data))
+    myBody();    
+    makeComboBoxOfShowsName(allShowsList.sort((a, b) => a.name.localeCompare(b.name)));    
+    getAllEpisodes();
+  })
+  .catch(error => console.log(error))
 }
 
 // myBody function will make all elements in body
@@ -27,7 +73,9 @@ function myBody(){
   // Create all elements
   body_El = document.querySelector('body');  
   searchDiv_El = document.createElement('div');
-  searchDivSelect_El = document.createElement('div');
+  showsDivSelect_El = document.createElement('div');
+  showsListSelect_El = document.createElement('select')
+  episodeDivSelect_El = document.createElement('div');
   episodeListSelect_El = document.createElement('select');
   searchDivInput_El = document.createElement('div');
   episodeInput_El = document.createElement('input');
@@ -40,8 +88,12 @@ function myBody(){
   body_El.className = 'body_CSS'
   searchDiv_El.id = 'searchDiv_JS';
   searchDiv_El.className = 'searchDiv_CSS';
-  searchDivSelect_El.id = 'searchDivSelect_JS';
-  searchDivSelect_El.className = 'searchDivSelect_CSS';
+  showsDivSelect_El.id = 'showsDivSelect_JS';
+  showsDivSelect_El.className = 'showsDivSelect_CSS';
+  showsListSelect_El.id = 'showsListSelect_JS';
+  showsListSelect_El.className = 'showsListSelect_CSS';
+  episodeDivSelect_El.id = 'episodeDivSelect_JS';
+  episodeDivSelect_El.className = 'episodeDivSelect_CSS';
   episodeListSelect_El.id = 'episodeListSelect_JS';
   episodeListSelect_El.className = 'episodeListSelect_CSS';
   searchDivInput_El.id = 'searchDivInput_JS';
@@ -61,41 +113,54 @@ function myBody(){
   // Add all elements to body
   body_El.appendChild(searchDiv_El);
   body_El.appendChild(mainDiv_El);
-  searchDiv_El.appendChild(searchDivSelect_El);
+  searchDiv_El.appendChild(showsDivSelect_El)
+  searchDiv_El.appendChild(episodeDivSelect_El);
   searchDiv_El.appendChild(searchDivInput_El);
-  searchDivSelect_El.appendChild(episodeListSelect_El);
+  showsDivSelect_El.appendChild(showsListSelect_El);
+  episodeDivSelect_El.appendChild(episodeListSelect_El);
   searchDivInput_El.appendChild(episodeInput_El);
   searchDivInput_El.appendChild(searchResualt_El);
+
+  // Select element on change event search in my object and find episod that his name is equal with value of select 
+  showsListSelect_El.addEventListener('change', ()=>{  
+    currentShowId = showsListSelect_El.value;
+    makePageForEpisodes(allEpisodesList.filter(ep => ep.showId == currentShowId), 'load');
+    episodeListSelect_El.innerHTML = '';
+    makeComboBoxOfEpisodesName(allEpisodesList.filter(el => el.showId == currentShowId));
+  });
   
   // Select element on change event search in my object and find episod that his name is equal with value of select 
   episodeListSelect_El.addEventListener('change', ()=>{  
-    (episodeListSelect_El.value == "allEepisodes") ? makePageForEpisodes(allEpisodes, 'load') : makePageForEpisodes(allEpisodes.filter(Episodes=> episodeListSelect_El.value == Episodes.name),'load');
+    (episodeListSelect_El.value == "allEepisodes") ? makePageForEpisodes(allEpisodesList.filter(ep => ep.showId == currentShowId), 'load') : makePageForEpisodes(allEpisodesList.filter(Episodes=> episodeListSelect_El.value == `${Episodes.name + ' - ' + titleCodeGenerator(Episodes)}`),'load');
   });
 
   // Input element on input event serach in my object and find all keyword that I wrote in input 
-  episodeInput_El.addEventListener("input", () => {
+  episodeInput_El.addEventListener("input", () => {        
     if (episodeInput_El.value != "") {    
-      highlight(episodeInput_El);
-      makePageForEpisodes(replacedObject, 'search');
       FoundIndexArray = [];     
-      replacedObject = [];        
-    } else {    
-      makePageForEpisodes(allEpisodes, 'load');
+      replacedObject = [];       
+      objectContainer = []; 
+      highlight(allEpisodesList.filter(el => el.showId == currentShowId), episodeInput_El);
+      makePageForEpisodes(replacedObject, 'search');      
+    } 
+    else {    
+      makePageForEpisodes(allEpisodesList.filter(ep => ep.showId == currentShowId), 'load')
       searchResualt_El.textContent = 'Result';
     }
   });
 }
 
 // Title generator function that have a array parameter , episode for example name:"Winter is Coming"  and episod:1 season:1 so episod resual should be "Winter is Coming - S01E01"
-function titleCodeGenerator(episode) {
+function titleCodeGenerator(episode) {  
   let seasonCode = episode.season < 10 ? "0" + episode.season : episode.season;
-  let episodeCode = episode.number < 10 ? "0" + episode.number : episode.number;
+  let episodeCode = episode.number < 10 ? "0" + episode.number : episode.number;  
   return `S${seasonCode}E${episodeCode}`;
 }
 // Summary in object mentioned <p> and </p> tags and these tags have to be removed
 function pureSummary(episode) {
+
   // Remove all <p> tags in sammary
-  let summaryStr = episode.summary.replace(/<p>/g, "");
+  let summaryStr = episode.summary.replace(/<p>/g, "");  
   // Remove all </p> tags in sammary and return it
   return summaryStr.replace(/<\/p>/g, "");
 }
@@ -116,6 +181,7 @@ function makePageForEpisodes(episodeList, searchType) {
     let episodeTitle_El = document.createElement('p');
     let episodeImg_El = document.createElement('img');
     let episodeSummaryLabel_El = document.createElement('strong');
+    let episodeSummaryDiv_El = document.createElement('div');
     let episodeSummary_El = document.createElement('p');
 
     episodeDiv_El.id = `episode_${index}_JS`;
@@ -124,10 +190,13 @@ function makePageForEpisodes(episodeList, searchType) {
     episodeTitle_El.className = 'episodeTitle_CSS';
     episodeImg_El.id = `episodeImg_${index}_JS`;
     episodeImg_El.className = 'episodeImg_CSS';
+    episodeSummaryDiv_El.id = `episodeSummaryDiv_${index}_Js`;
+    episodeSummaryDiv_El.className = `episodeSummaryDiv_CSS`;
     episodeSummaryLabel_El.id = `episodeSummaryLabel_${index}_Js`;
     episodeSummaryLabel_El.className = 'episodeSummaryLabel_CSS';
     episodeSummary_El.id = `episodeSummary_${index}_Js`;
     episodeSummary_El.className = 'episodeSummary_CSS';
+    
 
     episodeTitle_El.innerHTML = `${(searchType == 'search') ? episode.name : episode.name + ' - ' + titleCodeGenerator(episode)}`;
     episodeImg_El.src = `${episode.image.medium}`;
@@ -138,22 +207,33 @@ function makePageForEpisodes(episodeList, searchType) {
     episodeDiv_El.appendChild(episodeTitle_El)
     episodeDiv_El.appendChild(episodeImg_El)
     episodeDiv_El.appendChild(episodeSummaryLabel_El)
-    episodeDiv_El.appendChild(episodeSummary_El)    
-  });  
+    episodeDiv_El.appendChild(episodeSummaryDiv_El)
+    episodeSummaryDiv_El.appendChild(episodeSummary_El)    
+  });    
+}
+function makeComboBoxOfShowsName(allShowsList){  
+  allShowsList.forEach((episode, index) => {
+    options_JS = document.createElement('option');
+    options_JS.id = `showOption_${index}_JS`;
+    options_JS.className = `showOption_CSS`;
+    options_JS.value = `${episode.id}`;
+    options_JS.textContent = `${episode.name + ' - ' + titleCodeGenerator(episode)}`
+    showsListSelect_El.appendChild(options_JS);
+  });    
 }
 
 function makeComboBoxOfEpisodesName(episodeList){ 
   let options_JS = document.createElement('option'); 
   options_JS.id = `allEpisodes_JS`;
-  options_JS.className = `option_CSS`;
+  options_JS.className = `episodeOption_CSS`;
   options_JS.value = `allEepisodes`;
   options_JS.textContent = `All Episodes`;
   episodeListSelect_JS.appendChild(options_JS);  
   episodeList.forEach((episode, index) => {
     options_JS = document.createElement('option');
-    options_JS.id = `opthin_${index}_JS`;
-    options_JS.className = `opthin_CSS`;
-    options_JS.value = `${episode.name}`;
+    options_JS.id = `episodeOpthin_${index}_JS`;
+    options_JS.className = `episodeOpthin_CSS`;
+    options_JS.value = `${episode.name + ' - ' + titleCodeGenerator(episode)}`;
     options_JS.textContent = `${episode.name + ' - ' + titleCodeGenerator(episode)}`
     episodeListSelect_JS.appendChild(options_JS);
   });    
@@ -164,13 +244,10 @@ function getIndexes(stringToSearch, mainString, objectIndex, key) {
   let startIndex = 0;
   let newIndex;
   let stringToSearchLen = stringToSearch.length;
-  if (stringToSearchLen == 0) {
-    return [];
-  }
   mainString = mainString.toLowerCase();
   stringToSearch = stringToSearch.toLowerCase();
   while ((newIndex = mainString.indexOf(stringToSearch, startIndex)) > -1) {
-    FoundIndexArray.push({objectIndexes: objectIndex, objectKey: key, foundStrIndex: newIndex, keywordLength: stringToSearch.length});
+    FoundIndexArray.push({objectIndexes: objectIndex, objectKey: key, foundStrIndex: newIndex});
     startIndex = newIndex + stringToSearchLen;
     findState = true;
   }
@@ -188,33 +265,33 @@ function nameAndSummaryHighlight(objectContainer, nameOrSummary, nameOrSummaryKe
   nameOrSummaryDividedArray.forEach((element, i) => {
     (i < nameOrSummaryDividedArray.length - 1) ? (nameOrSummaryDividedArray[i] = `${nameOrSummaryDividedArray[i].slice(0, nameOrSummaryDividedArray[i].length - elementParameterToSearch.value.length)}<strong class="highlight_CSS">${nameOrSummaryDividedArray[i].slice(nameOrSummaryDividedArray[i].length - elementParameterToSearch.value.length)}</strong>`) : {};
   });
-  (nameOrSummaryKey == 'name') ? objectContainer.name = nameOrSummaryDividedArray.join("") : objectContainer.summary = nameOrSummaryDividedArray.join("");       
+  (nameOrSummaryKey == 'name') ? objectContainer.name = nameOrSummaryDividedArray.join("") : objectContainer.summary = nameOrSummaryDividedArray.join("");   
+
 }
 
-function highlight(elementParameterToSearch) {
-  let myAllEpisodes = [];
-  let totalFoundObject = 0;
-  myAllEpisodes = getAllEpisodes();     
-  myAllEpisodes.forEach((episode, index) => {
-    episode.name = `${episode.name} - ${titleCodeGenerator(episode)}`;
-    episode.summary = `${pureSummary(episode)}`;
-    let nameState = getIndexes(elementParameterToSearch.value, `${episode.name}`, index, "name");
-    let summaryState = getIndexes(elementParameterToSearch.value, `${episode.summary}`, index, "summary");        
-    if (nameState || summaryState) {
-      let objectContainer = episode;      
+function highlight(episodeList, elementParameterToSearch) {
+  let totalFoundObject = 0;      
+  episodeList.forEach((episode, index) => {
+    let nameContainer = `${episode.name} - ${titleCodeGenerator(episode)}`;    
+    let summaryContainer = `${pureSummary(episode)}`;    
+    let nameState = getIndexes(elementParameterToSearch.value, `${nameContainer}`, index, "name");
+    let summaryState = getIndexes(elementParameterToSearch.value, `${summaryContainer}`, index, "summary");        
+    if (nameState || summaryState) {   
+      objectContainer = [];   
+      objectContainer = JSON.parse(JSON.stringify(episode))      
       totalFoundObject += 1;
       // ------------------------------------------------------ Name Highlight ---------------------------------------------------------
       if (nameState) {        
-        nameAndSummaryHighlight(objectContainer, objectContainer.name, 'name', index, elementParameterToSearch);            
+        nameAndSummaryHighlight(objectContainer, nameContainer, 'name', index, elementParameterToSearch);            
       }
       // ------------------------------------------------------ Summary Highlight ------------------------------------------------------
       if (summaryState) {        
-        nameAndSummaryHighlight(objectContainer, objectContainer.summary, 'summary', index, elementParameterToSearch);        
+        nameAndSummaryHighlight(objectContainer, summaryContainer, 'summary', index, elementParameterToSearch);        
       }
       replacedObject.push(objectContainer);      
     }
   });  
-  searchResualt_El.textContent = `${totalFoundObject} | ${myAllEpisodes.length}`;
+  searchResualt_El.textContent = `${totalFoundObject} | ${episodeList.length}`;
 }
 
 window.onload = setup;
